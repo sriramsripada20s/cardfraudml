f3f8189f123940904802e2dc84871f2a87efb3ce
sripadas20 <sripadas@usf.edu>
2023-11-21 20:08:26 +0530

    modeltrainer.py added

---
+++ src/fraud_detection/components/model_trainer.py
@@ -0,0 +1,30 @@
+import os
+from fraud_detection import logger
+#from sklearn.preprocessing import LabelEncoder
+from sklearn.tree import DecisionTreeClassifier
+import pandas as pd
+import joblib
+from fraud_detection.entity.config_entity import ModelTrainerConfig
+
+class ModelTrainer:
+    def __init__(self, config: ModelTrainerConfig):
+        self.config = config
+
+    
+    def train(self):
+        train_data = pd.read_csv(self.config.train_data_path)
+        test_data = pd.read_csv(self.config.test_data_path)
+
+
+        train_x = train_data.drop([self.config.target_column], axis=1)
+        test_x = test_data.drop([self.config.target_column], axis=1)
+        train_y = train_data[[self.config.target_column]]
+        test_y = test_data[[self.config.target_column]]
+
+
+        dt = DecisionTreeClassifier(min_impurity_decrease=self.config.min_impurity_decrease,min_samples_leaf=self.config.min_samples_leaf,
+                                    min_samples_split=self.config.min_samples_split,random_state=42)
+        dt.fit(train_x, train_y.values.ravel())
+
+        joblib.dump(dt, os.path.join(self.config.root_dir, self.config.model_name))
+    
