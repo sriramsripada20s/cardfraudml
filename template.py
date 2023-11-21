059769649a052c66532efa9054057e1e9c6f0f9a
sripadas20 <sripadas@usf.edu>
2023-11-21 13:27:22 +0530

    added requirements.txt, template.py

--- template.py
+++ template.py
@@ -7,7 +7,7 @@ logging.basicConfig(level=logging.INFO,format='[%(asctime)s]: %(message)s:')
 
 project_name= "fraud_detection"
 
-list_of_files =[
+list_of_files = [
        ".github/workflows/.gitkeep",
     f"src/{project_name}/__init__.py",
     f"src/{project_name}/components/__init__.py",
