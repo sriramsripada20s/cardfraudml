1379b67729da08930843b030f403831862525110
sripadas20 <sripadas@usf.edu>
2023-11-21 23:57:33 +0530

    deleted some files

--- src/fraud_detection/constants/__init__.py
+++
@@ -1,6 +0,0 @@
-from pathlib import Path
-
-# File paths
-CONFIG_FILE_PATH = Path("config/config.yaml")  # Path to the configuration file
-PARAMS_FILE_PATH = Path("params.yaml")  # Path to the parameters file
-SCHEMA_FILE_PATH = Path("schema.yaml")  # Path to the schema file
