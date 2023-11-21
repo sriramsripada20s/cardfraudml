c1ff6f934ead9f96fb3fc330f5ff69883c27e1bf
sripadas20 <sripadas@usf.edu>
2023-11-22 00:28:36 +0530

    added comments in configuration.py

--- src/frauddetection/config/configuration.py
+++ src/frauddetection/config/configuration.py
@@ -3,7 +3,6 @@ from frauddetection.utils.common import read_yaml, create_directories
 
 from frauddetection.entity.config_entity import (DataIngestionConfig,DataValidationConfig,DataTransformationConfig,ModelTrainerConfig,ModelEvaluationConfig)
 
-
 class ConfigurationManager:
     def __init__(
         self,
