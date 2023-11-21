92eee0b4844737304f4125c239c5c5725770a57c
sripadas20 <sripadas@usf.edu>
2023-11-22 00:24:57 +0530

    added comments

--- src/frauddetection/utils/common.py
+++ src/frauddetection/utils/common.py
@@ -1,7 +1,7 @@
 import os
 from box.exceptions import BoxValueError
 import yaml
-from fraud_detection import logger
+from frauddetection import logger
 import json
 import joblib
 from ensure import ensure_annotations
